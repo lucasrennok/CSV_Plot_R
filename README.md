b0e2b5247ba3207d318f34752e682388a0485221
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-08-15 03:37:04 -0300

    readme updated & library commented

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-# Read a CSV
+# :pencil2: Read a CSV
 This repository: a small piece of code that reads a CSV file and plot it in charts(made by R) and print all the data frame at the end of the program.
+
+> Plot:
+>
+> ![plot](/images/plot.PNG)
+
+> CSV printed:
+>
+> ![print csv](/images/printPlot.PNG)
 
 Use your only CSV to test it.
 
-I made this repository to test how to read a CSV file in R.
+:bookmark_tabs: I made this repository to test how to read a CSV file in R.
--- csv_reader.R
+++ csv_reader.R
@@ -1,18 +1,18 @@
 
 #Packages
 install.packages("ggplot2", dependencies=TRUE)
-install.packages("dplyr", dependencies=TRUE)
-install.packages("tidyr", dependencies=TRUE)
-install.packages("tidyverse", dependencies=TRUE)
+# install.packages("dplyr", dependencies=TRUE)
+# install.packages("tidyr", dependencies=TRUE)
+# install.packages("tidyverse", dependencies=TRUE)
 
 #Library
 library(ggplot2)
-library(dplyr)
-library(tidyr)
-library(tidyverse)
+# library(dplyr)
+# library(tidyr)
+# library(tidyverse)
 
 # Read in csv files
-df <- read.csv("./csv_file_name_here.csv", header=T, sep=",")
+df <- read.csv("./NDHUB.TraumaCenters_0.csv", header=T, sep=",")
 
 # Select the first five columns
 # df %>% select(1:5)
